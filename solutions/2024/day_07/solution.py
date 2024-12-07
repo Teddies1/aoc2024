1a6ce8f78befe53fc5facbbf8dac6f0beff0e55b
Teddies1 <nghonjoo@gmail.com>
2024-12-07 18:46:47 +0800

    added early break for optimsation

--- solutions/2024/day_07/solution.py
+++ solutions/2024/day_07/solution.py
@@ -4,6 +4,7 @@
 
 from ...base import StrSplitSolution, answer
 import itertools
+import time
 
 class Solution(StrSplitSolution):
     _year = 2024
@@ -11,13 +12,14 @@ class Solution(StrSplitSolution):
 
     # @answer(1234)
     def part_1(self) -> int:
+        start = time.time()
         ans = set()
         for inp in self.input:
             inp = inp.split(":")
             value = int(inp[0])
             operands = inp[1].split(" ")[1:]
             operator_length = len(operands) - 1            
-            operators = ["+", "*"]
+            operators = [0, 1]
             
             for ops in itertools.product(operators, repeat=operator_length):
                 operand_pointer = 0
@@ -26,30 +28,39 @@ class Solution(StrSplitSolution):
                     num1 = int(stack.pop())
                     num2 = int(stack.pop())
                     intermediate = 0
-                    if ops[operand_pointer] == "*":
+                    if ops[operand_pointer] == 1:
                         intermediate += num1 * num2
                         stack.append(intermediate)
                         operand_pointer += 1
+                        if intermediate > value:
+                            break
                     else:
                         intermediate += num1 + num2
                         stack.append(intermediate)
                         operand_pointer += 1
+                        if intermediate > value:
+                            break
+                        
                 if stack[0] == value:
                     ans.add(stack[0])   
-                
+                    break
+        end = time.time()
+        length = end - start
+        print("It took", length, "seconds!")           
         return sum(ans)
             
             
 
     # @answer(1234)
     def part_2(self) -> int:
+        start = time.time()
         ans = set()
         for inp in self.input:
             inp = inp.split(":")
             value = int(inp[0])
             operands = inp[1].split(" ")[1:]
             operator_length = len(operands) - 1            
-            operators = ["+", "*", "||"]
+            operators = [0, 1, 2]
             
             for ops in itertools.product(operators, repeat=operator_length):
                 operand_pointer = 0
@@ -58,11 +69,11 @@ class Solution(StrSplitSolution):
                     num1 = int(stack.pop())
                     num2 = int(stack.pop())
                     intermediate = 0
-                    if ops[operand_pointer] == "*":
+                    if ops[operand_pointer] == 1:
                         intermediate += num1 * num2
                         stack.append(intermediate)
                         operand_pointer += 1
-                    elif ops[operand_pointer] == "||":
+                    elif ops[operand_pointer] == 2:
                         intermediate = str(num1) + str(num2)
                         stack.append(int(intermediate))
                         operand_pointer += 1
@@ -72,7 +83,11 @@ class Solution(StrSplitSolution):
                         operand_pointer += 1
                 if stack[0] == value:
                     ans.add(stack[0])   
+                    break
                 
+        end = time.time()
+        length = end - start
+        print("It took", length, "seconds!")
         return sum(ans)
 
     # @answer((1234, 4567))
